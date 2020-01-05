b13a4548381594bf3aacd9d0ed45f84c30c4efca
DevRappers <devrappers@gmail.com>
2020-01-05 19:24:38 +0900

    aws RDS 연동 및 api수정

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,33 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/customers', (req, res) => {
-	res.send([
-		{
-			id: 1,
-			image: 'https://placeimg.com/64/64/1',
-			name: '이재준',
-			birthday: '980326',
-			gender: '남자',
-			job: 'developer'
-		},
-		{
-			id: 2,
-			image: 'https://placeimg.com/64/64/2',
-			name: '테스트유저',
-			birthday: '910208',
-			gender: '여자',
-			job: '디자이너'
-		},
-		{
-			id: 3,
-			image: 'https://placeimg.com/64/64/3',
-			name: '이재영',
-			birthday: '950815',
-			gender: '남자',
-			job: '직장인'
-		}
-	]);
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+
+const connection = mysql.createConnection({
+	host: conf.host,
+	user: conf.user,
+	password: conf.password,
+	port: conf.port,
+	database: conf.database
+});
+connection.connect();
+
+app.get('/api/webtoons', (req, res) => {
+	connection.query('SELECT * FROM WEBTOON', (err, rows, fields) => {
+		res.send(rows);
+	});
 });
 
 app.listen(port, () => console.log(`웹툰 서버 동작중 http://localhost:${port}`));
