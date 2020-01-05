03cd799d44e8fc9a33ee2b3169c891d0e819cb06
DevRappers <devrappers@gmail.com>
2020-01-05 15:48:21 +0900

    기본 api개발

--- server.js
+++ server.js
@@ -3,11 +3,37 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// json형식으로 하겠다.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-	res.send({ message: 'Hello Express!' });
+app.get('/api/customers', (req, res) => {
+	res.send([
+		{
+			id: 1,
+			image: 'https://placeimg.com/64/64/1',
+			name: '이재준',
+			birthday: '980326',
+			gender: '남자',
+			job: 'developer'
+		},
+		{
+			id: 2,
+			image: 'https://placeimg.com/64/64/2',
+			name: '테스트유저',
+			birthday: '910208',
+			gender: '여자',
+			job: '디자이너'
+		},
+		{
+			id: 3,
+			image: 'https://placeimg.com/64/64/3',
+			name: '이재영',
+			birthday: '950815',
+			gender: '남자',
+			job: '직장인'
+		}
+	]);
 });
 
-app.listen(port, () => console.log(`웹툰 서버 독장중 http://localhost:${port}`));
+app.listen(port, () => console.log(`웹툰 서버 동작중 http://localhost:${port}`));
